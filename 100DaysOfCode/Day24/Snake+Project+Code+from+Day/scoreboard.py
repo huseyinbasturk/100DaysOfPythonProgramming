71b787b6bc1c895eaa49ed47c1fbec8689b52fe2
huseyinbasturk <hb.basturk41@gmail.com>
2024-02-13 23:46:23 +0100

    highest score  stored in file system

--- 100DaysOfCode/Day24/Snake+Project+Code+from+Day/scoreboard.py
+++ 100DaysOfCode/Day24/Snake+Project+Code+from+Day/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -19,13 +20,16 @@ class Scoreboard(Turtle):
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+
